3d4f9d8792861a47d0fb074cfd57e7c59c6fa692
Juan Batty <jmbl1685@hotmail.com>
2023-08-09 09:32:06 -0500

    more updates

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
-import getActiv from './lib/activ/activ';
+
+import {
+	getActiv,
+} from './lib/activ/activ';
 
 import { config } from './config/config';
 
 import * as utils from './utils/utils';
 import { v4 } from '@ixily/activ';
-
-import { ImagesModule } from '@ixily/activ/dist/src/modules/activ-v4';
 
 //@ts-ignore
 const app = express();
@@ -20,6 +21,15 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 
 let strategyReference: string = null as any;
+
+const GUESTS_ADDRESSES = [
+	'0x00d2ADa3365e40C05E8772e58B9aAcf356F3E474', // Dario
+	'0xc4Cd878B9c5F1D968c3BEC5fe39A10ad8A80C973', // Gaspar
+	'0x2767441E044aCd9bbC21a759fB0517494875092d', // Cynan
+	'0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65', // Marcelo
+	'0xa0Ee7A142d267C1f36714E4a8F75612F20a79720', // LC
+	'0x90F79bf6EB2c4f870365E785982E1f101E93b906', // LR
+];
 
 app.get(
 	'/v1/activ/ideas',
@@ -83,6 +93,9 @@ app.get(
 app.get(
 	'/v1/activ/ideas/create',
 	async (req: express.Request, res: express.Response) => {
+
+		let raw: any = {};
+
 		try {
 			const request = {
 				...req.body,
@@ -97,71 +110,61 @@ app.get(
 			const tickerDescription = ticker;
 
 			// change this to your wallet address
-			const creatorWallet = '0xaB31A127b112CcF2e97fC54A842A6a3b7070BEa9';
+			const creatorWallet = '0x00d2ADa3365e40C05E8772e58B9aAcf356F3E474';
 
-			const strategyLogo = await ImagesModule.convertOrMinifyImageToBase64(
-				'https://loremflickr.com/1920/500/abstract',
-				'banner'
-			);
+			strategyReference = request?.strategyReference || null;
 
-			const companyLogo = await ImagesModule.convertOrMinifyImageToBase64(
-				'https://loremflickr.com/250/250/abstract',
-				'profile'
-			);
+			if (!strategyReference) {
+				strategyReference = v4.generateUUID();
+			}
+
+			const strategyInfo = {
+				reference: strategyReference,
+				name: 'Random Strategy',
+				description:
+					'Strategy created to test the functionality of the Activ SDK',
+			};
 
 			const tickerInfo = {
 				ticker,
 				tickerIconBase64: await utils.getTickerIcon(ticker),
 				description: tickerDescription,
-			}
+			};
 
-			strategyReference = request?.strategyReference || null;
-			
-			if (!strategyReference) {
-				strategyReference = v4.generateUUID();
-			}
-
-			const newIdea: v4.ITradeIdea = {
+			const IDEA_CREATE_PAYLOAD: v4.ITradeIdea = {
 				content: {
 					reference: v4.generateUUID(),
 				},
 				strategy: {
-					creatorWallet,
-					reference: strategyReference,
-					name: 'Random Strategy',
-					description: 'Strategy created to test and debug',
-					creatorName: '',
+					creatorWallet: creatorWallet,
+					reference: strategyInfo.reference,
+					name: strategyInfo.name,
+					description: strategyInfo.description,
+					creatorName: 'Random',
 					image: {
-						b64: strategyLogo,
+						url: 'https://ixily.io/assets/img/profile/activ-profile-default.png',
 					}
 				},
 				creator: {
-					company: 'R Company',
-					name: 'Random Creator',
-					url: 'https://example.com',
+					company: 'random.io',
+					name: 'Random',
+					url: 'https://random.io',
 					walletAddress: creatorWallet,
 					companyLogo: {
-						b64: companyLogo,
+						url: 'https://avatars.githubusercontent.com/u/131881524?s=200&v=4',
 					}
 				},
 				access: {
-					wallets: [
-						'0x00d2ADa3365e40C05E8772e58B9aAcf356F3E474', // Dario
-						'0xc4Cd878B9c5F1D968c3BEC5fe39A10ad8A80C973', // Gaspar
-						'0x2767441E044aCd9bbC21a759fB0517494875092d', // Cynan
-						'0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65', // Marcelo
-						'0xa0Ee7A142d267C1f36714E4a8F75612F20a79720', // LC
-						'0x90F79bf6EB2c4f870365E785982E1f101E93b906', // LR
-					],
+					wallets: GUESTS_ADDRESSES,
 				},
 				idea: {
-					title: tickerInfo.ticker,
 					kind: 'open',
+					title: 'Trade Idea from ACTIV Automated Test',
 					asset: {
 						ticker: tickerInfo.ticker,
 						description: tickerInfo.description,
 						image: {
-							b64: tickerInfo.tickerIconBase64
+							b64: tickerInfo.tickerIconBase64,
 						}
 					},
 					trade: {
@@ -169,30 +172,31 @@ app.get(
 						direction: request?.direction || 'long',
 					},
 					notes: {
-						commentary: 'This is a test idea',
+						commentary:
+							'This trade idea was opened automatically by "ACTIV Automated Test" to test the functionality and ensure it works as expected.',
 					},
 				},
 				pricing: {
 					provider,
 				},
-				isPublic: false,
-				devSettings: {
-					environment: 'dev'
-				}
-			};
+			}
 
-			const ideaCreated = await activ.createIdea(newIdea);
+			raw.idea = IDEA_CREATE_PAYLOAD;
+
+			const ideaCreated = await activ.createIdea(IDEA_CREATE_PAYLOAD);
 
 			return res.json({
 				data: ideaCreated,
 				message: 'ideas created',
 				status: 'success',
-			})
+			});
+
 		} catch (err: any) {
 			return res.json({
 				message: err.message,
 				status: 'failure',
-			})
+				raw,
+			});
 		}
 	},
 );
@@ -206,7 +210,7 @@ app.get(
 				...req.body,
 				...req.query,
 				...req.params,
-			}
+			};
 
 			const activ = await getActiv();
 
@@ -219,7 +223,7 @@ app.get(
 				strategyReference = v4.generateUUID();
 			}
 
-			const ideaToClose: v4.ITradeCloseIdea = {
+			const IDEA_CLOSE_PAYLOAD: v4.ITradeCloseIdea = {
 				ticker,
 				strategyReference,
 				pricingCredentials: {
@@ -227,18 +231,19 @@ app.get(
 				},
 			}
 
-			const ideaClosed = await activ.closeIdea(ideaToClose);
+			const ideaClosed = await activ.closeIdea(IDEA_CLOSE_PAYLOAD);
 
 			return res.json({
 				data: ideaClosed,
 				message: 'idea closed',
 				status: 'success',
-			})
+			});
+
 		} catch (err: any) {
 			return res.json({
 				message: err.message,
 				status: 'failure',
-			})
+			});
 		}
 	},
 );
--- src/lib/activ/activ.ts
+++ src/lib/activ/activ.ts
@@ -1,36 +1,37 @@
+import 'isomorphic-fetch'
 import 'dotenv/config';
 
 import {
     LitNodeProviderModule,
     CacheNodeStorageModule as CacheStorageModule,
     v4,
-} from '@ixily/activ';
+} from '@ixily/activ'
 
 const {
     ActivV4Module,
-} = v4;
+} = v4
 
 const activ = ActivV4Module;
 
 export type IActiv = typeof activ;
 
 //@ts-ignore
-import * as LitJsSdk from '@lit-protocol/lit-node-client-nodejs';
+import * as LitJsSdk from '@lit-protocol/lit-node-client-nodejs'
 //@ts-ignore
 import * as Siwe from "siwe";
 //@ts-ignore
-import * as Jimp from 'jimp';
+import * as Jimp from 'jimp'
 
 import {
     EnvModule,
     getBoolean
-} from '@ixily/activ/dist/src/modules/activ-v4'
+} from '@ixily/activ/dist/src/modules/activ-v4';
 
-const nftStorageKey = process.env.NFT_STORAGE_KEY
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const NFT_STORAGE_KEY = process.env.NFT_STORAGE_KEY as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 const showLogsToDebugConfig = (): boolean => {
-    return process.env.APP_ENV === 'production' ? false : false
+    return process.env.APP_ENV === 'production' ? false : false;
 }
 
 const MUMBAI_CONFIG: v4.IActivConfig = {
@@ -44,7 +45,7 @@ const MUMBAI_CONFIG: v4.IActivConfig = {
         mock: false,
     },
     mockProvableValidation: false,
-    nftStorageKey,
+    nftStorageKey: NFT_STORAGE_KEY,
     mockNftStorage: false,
     ipfsProxyEnabled: true,
     showLogsToDebug: showLogsToDebugConfig(),
@@ -52,169 +53,60 @@ const MUMBAI_CONFIG: v4.IActivConfig = {
         isBrowser: false,
         module: CacheStorageModule,
         dbParams: {
-            provider: 'memory',
+            provider: 'none',
         },
-        useCache: true,
+        useCache: false,
     },
 }
 
-const GOERLI_CONFIG: v4.IActivConfig = {
-    defaultBlockchainNetwork: 'goerli',
-    defaultContract: 'v4',
-    defaultContractOptions: {
-        userWalletPrivateKey: PRIVATE_KEY,
-    },
-    litConfig: {
-        litProvider: LitNodeProviderModule,
-        mock: false,
-    },
-    nftStorageKey,
-    mockNftStorage: false,
-    skipPricingSignature: false,
-    ipfsProxyEnabled: true,
-    showLogsToDebug: showLogsToDebugConfig(),
-    cacheStorageConfig: {
-        isBrowser: false,
-        module: CacheStorageModule,
-        dbParams: {
-            provider: 'memory',
-        },
-        useCache: true,
-    },
-}
-
-const SEPOLIA_CONFIG: v4.IActivConfig = {
-    defaultBlockchainNetwork: 'sepolia',
-    defaultContract: 'v4',
-    defaultContractOptions: {
-        userWalletPrivateKey: PRIVATE_KEY,
-    },
-    litConfig: {
-        litProvider: LitNodeProviderModule,
-        mock: false,
-    },
-    nftStorageKey,
-    mockNftStorage: false,
-    skipPricingSignature: false,
-    ipfsProxyEnabled: true,
-    showLogsToDebug: showLogsToDebugConfig(),
-    cacheStorageConfig: {
-        isBrowser: false,
-        module: CacheStorageModule,
-        dbParams: {
-            provider: 'memory',
-        },
-        useCache: true,
-    },
-}
-
-const POLYGON_CONFIG: v4.IActivConfig = {
-    defaultBlockchainNetwork: 'polygon',
-    defaultContract: 'v4',
-    defaultContractOptions: {
-        userWalletPrivateKey: PRIVATE_KEY,
-    },
-    litConfig: {
-        litProvider: LitNodeProviderModule,
-        mock: false,
-    },
-    nftStorageKey,
-    mockNftStorage: false,
-    skipPricingSignature: false,
-    ipfsProxyEnabled: true,
-    showLogsToDebug: showLogsToDebugConfig(),
-    cacheStorageConfig: {
-        isBrowser: false,
-        module: CacheStorageModule,
-        dbParams: {
-            provider: 'memory',
-        },
-        useCache: true,
-    },
-}
-
-export type NetworkType = 'goerli' | 'mumbai' | 'sepolia' | 'polygon';
+export type NetworkType = 'mumbai';
 
 const state = {
     configured: {
-        goerli: false as boolean,
         mumbai: false as boolean,
     },
     instance: {
-        goerli: null as any,
-        mumbai: null as any,
+        mumbai: (null as any) as string,
     },
     privateKey: {
-        goerli: null as any,
-        mumbai: null as any,
+        mumbai: (null as any) as string,
     }
 }
 
-const getActiv = async (
+export const getActiv = async (
     network: NetworkType = 'mumbai',
 ): Promise<typeof activ> => {
-    // @ts-ignore
     if (!getBoolean(state.configured[network])) {
 
         const initObj = {
             LitJsSdkInstance: LitJsSdk,
             SiweInstance: Siwe,
-            backendWalletPrivateKey: null,
-        };
+            backendWalletPrivateKey: '',
+        }
 
         switch (network) {
-            case 'goerli':
-                // @ts-ignore
-                initObj.backendWalletPrivateKey = PRIVATE_KEY;
-                await (LitNodeProviderModule as any).init(initObj);
-                await v4.ImagesModule.init({ JimpInstance: Jimp });
-                await EnvModule.set('isProd', false);
-                await activ.config(GOERLI_CONFIG);
-                break
             case 'mumbai':
-                // @ts-ignore
-                initObj.backendWalletPrivateKey = PRIVATE_KEY
-                await (LitNodeProviderModule as any).init(initObj)
-                await v4.ImagesModule.init({ JimpInstance: Jimp })
-                await EnvModule.set('isProd', false)
-                await activ.config(MUMBAI_CONFIG)
-                break
-            case 'sepolia':
-                // @ts-ignore
                 initObj.backendWalletPrivateKey = PRIVATE_KEY;
                 await (LitNodeProviderModule as any).init(initObj);
                 await v4.ImagesModule.init({ JimpInstance: Jimp });
                 await EnvModule.set('isProd', false);
-                await activ.config(SEPOLIA_CONFIG);
-                break
-            case 'polygon':
-                // @ts-ignore
-                initObj.backendWalletPrivateKey = PRIVATE_KEY;
-                await (LitNodeProviderModule as any).init(initObj);
-                await v4.ImagesModule.init({ JimpInstance: Jimp });
-                await EnvModule.set('isProd', true);
-                await activ.config(POLYGON_CONFIG);
-                break
+                await activ.config(MUMBAI_CONFIG);
+                break;
         }
 
         (state as any).configured[network] = true;
         (state as any).instance[network] = activ;
         (state as any).privateKey[network] = initObj.backendWalletPrivateKey;
-    };
+
+    }
 
     const networkChainObj = {
-        goerli: 'goerli',
         mumbai: 'mumbai',
-        sepolia: 'sepolia',
-        polygon: 'polygon',
     };
 
     await activ.selectChainContract(networkChainObj[network], 'v4', {
-        // @ts-ignore
         userWalletPrivateKey: state.privateKey[network],
-    });
+    })
 
-    return activ;
+    return activ
 }
-
-export default getActiv;
